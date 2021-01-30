df501ef2cb67b8a56b65ac19a9b61900827d1c74
Tim Cheung <tim@cheung.io>
2021-01-30 17:20:35 +0000

    add more tools

--- tools/init.sh
+++ tools/init.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+git clone https://github.com/corpnewt/MountEFI
+git clone https://github.com/corpnewt/ProperTree
 git clone https://github.com/corpnewt/GenSMBIOS
+git clone https://github.com/corpnewt/OCConfigCompare
+git clone https://github.com/corpnewt/Lilu-and-Friends
+git clone https://github.com/corpnewt/KextExtractor

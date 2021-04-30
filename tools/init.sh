b01f21d6f63b635e82ff196e3ee2f5e3ba869eda
Tim Cheung <tim@cheung.io>
2021-04-30 17:13:28 +0100

    add `USBMap`

--- tools/init.sh
+++ tools/init.sh
@@ -6,3 +6,4 @@ git clone https://github.com/corpnewt/GenSMBIOS
 git clone https://github.com/corpnewt/OCConfigCompare
 git clone https://github.com/corpnewt/Lilu-and-Friends
 git clone https://github.com/corpnewt/KextExtractor
+git clone https://github.com/corpnewt/USBMap
